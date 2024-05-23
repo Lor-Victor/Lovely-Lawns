76650e329ecab04e819e9d9cd05b75de2cc56fe3
Victor Lor <vylor@uncg.edu>
2024-05-22 20:42:12 -0400

    added announcement button

--- adminscript.js
+++ adminscript.js
@@ -8,10 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputContent = document.getElementById('inputcontent');
     const submitBtn = document.getElementById('submitbtn');
     const forumsContainer = document.getElementById('forums');
+    const newAnnounceBtn = document.getElementById('announcebtn');
     const repbtn = document.getElementById('repbtn')
     let isCreatingPost = false;
 //divide the middle section better and make forums open new page with entire forum
 //add reply btns and add view more replies which opens to entire post
+
+    newAnnounceBtn.addEventListener('click', () => {
+        modalTitle.textContent = 'Create Announcement';
+        inputTitle.value = '';
+        inputContent.value = '';
+        modal.style.display = 'flex';
+        isCreatingPost = true;
+    });
 
     newPostBtn.addEventListener('click', () => {
         modalTitle.textContent = 'Create New Post';
